0e2f4df686f75b30aa5ef6f3414f0e885f7cb875
Rodrigo Addor <me@rodrick.dev>
2022-07-11 14:27:25 -0300

    feat: improve gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,22 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val transitiveInclude: Configuration by configurations.creating {
+    exclude(group = "org.jetbrains.kotlin")
+    exclude(group = "org.jetbrains.kotlinx")
+    exclude(group = "com.mojang")
+}
+
 plugins {
-    id("fabric-loom")
-    id("maven-publish")
+    id("fabric-loom") version "0.12-SNAPSHOT"
 
     val kotlinVersion: String by System.getProperties()
     kotlin("jvm").version(kotlinVersion)
     kotlin("plugin.serialization").version(kotlinVersion)
+}
+
+repositories {
+    mavenCentral()
+    maven("https://maven.fabricmc.net/")
 }
 
 base {
@@ -15,59 +25,54 @@ base {
 }
 
 val modVersion: String by project
-version = modVersion
 val mavenGroup: String by project
+version = modVersion
 group = mavenGroup
 
-repositories {
-    mavenCentral()
-}
+val minecraftVersion: String by project
+val yarnMappings: String by project
+val loaderVersion: String by project
+val fabricVersion: String by project
+val fabricKotlinVersion: String by project
 
 dependencies {
-    val minecraftVersion: String by project
     minecraft("com.mojang", "minecraft", minecraftVersion)
-
-    val yarnMappings: String by project
     mappings("net.fabricmc", "yarn", yarnMappings, null, "v2")
-
-    val loaderVersion: String by project
     modImplementation("net.fabricmc", "fabric-loader", loaderVersion)
 
-    val fabricVersion: String by project
     modImplementation("net.fabricmc.fabric-api", "fabric-api", fabricVersion)
 
-    val fabricKotlinVersion: String by project
-    modImplementation("net.fabricmc", "fabric-language-kotlin", fabricKotlinVersion)
+    include(modImplementation("net.fabricmc", "fabric-language-kotlin", fabricKotlinVersion))
 
-    implementation("com.charleskorn.kaml", "kaml", "0.46.0")
+    transitiveInclude(implementation("com.charleskorn.kaml:kaml:0.46.0")!!)
+
+    transitiveInclude.resolvedConfiguration.resolvedArtifacts.forEach {
+        include(it.moduleVersion.id.toString())
+    }
 }
 
 tasks {
-    val javaVersion = JavaVersion.VERSION_17
-    withType<JavaCompile> {
-        options.encoding = "UTF-8"
-        sourceCompatibility = javaVersion.toString()
-        targetCompatibility = javaVersion.toString()
-        options.release.set(javaVersion.toString().toInt())
-    }
+    processResources {
+        inputs.properties(
+            "version" to project.version
+        )
 
-    val kotlinVersion = "1.7"
-    withType<KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = javaVersion.toString()
-            apiVersion = kotlinVersion
-            languageVersion = kotlinVersion
+        filesMatching("fabric.mod.json") {
+            expand("version" to project.version)
         }
     }
 
-    jar { from("LICENSE") { rename { "${it}_${base.archivesName}" } } }
-    processResources {
-        inputs.property("version", project.version)
-        filesMatching("fabric.mod.json") { expand(mutableMapOf("version" to project.version)) }
+    val javaVersion = JavaVersion.VERSION_17
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.release.set(javaVersion.toString().toInt())
+    }
+
+    withType<KotlinCompile> {
+        kotlinOptions { jvmTarget = javaVersion.toString() }
     }
 
     java {
-        toolchain { languageVersion.set(JavaLanguageVersion.of(javaVersion.toString())) }
         sourceCompatibility = javaVersion
         targetCompatibility = javaVersion
         withSourcesJar()
