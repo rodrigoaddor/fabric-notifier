af64023ee94ffc36b10b60b08ff2bb51d1fa43bc
Rodrigo Addor <me@rodrick.dev>
2022-07-17 23:56:11 -0300

    ci: change gradle kotlin version strategy

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,15 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.namespace?.startsWith("org.jetbrains.kotlin") == true) {
+                val kotlinVersion: String by settings
+                useVersion(kotlinVersion)
+            }
+        }
+    }
 }
 
 include("gen")
