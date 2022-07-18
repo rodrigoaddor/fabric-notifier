9654c7a6381cd7b8cf7f61c96983651d2cfeb5a3
Rodrigo Addor <me@rodrick.dev>
2022-07-18 00:01:21 -0300

    ci: remove kotlinversion variable

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,15 +4,6 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
-
-    resolutionStrategy {
-        eachPlugin {
-            if (requested.id.namespace?.startsWith("org.jetbrains.kotlin") == true) {
-                val kotlinVersion: String by settings
-                useVersion(kotlinVersion)
-            }
-        }
-    }
 }
 
 include("gen")
